0ba94a40dfb1906c823d66dba8e70d9188afe0ca
Anjali Jain <jainanja@northeastern.edu>
2022-01-29 18:56:38 -0500

    merge with remote master

---
+++ CarApplication/src/model/Person.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anjali
+ */
+public class Person {
+    
+    
+    private String brand;
+    private String model;
+    private String color;
+    private String year;
+    private String engineNo;
+    private ArrayList<String> seatsNumber;
+    private ArrayList<String> licensePlate;
+    private String ownerName;
+    private String ownerTelephonenumbers;
+    private String ownerEmailAddresses;
+    private String ownerDriverLicense;
+    private String ownerSocialSecurityNumber;
+    private String ownerAddress;
+    private String serviceRecords;
+    private String warrantyYear;
+    private String photo;
+    
+}
